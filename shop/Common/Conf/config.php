019f4a403da28fb544e71e5c25a3160d8a82406c
levi <levi@huaxi100.com>
2017-02-21 18:06:05 +0800

    init项目框架

---
+++ shop/Common/Conf/config.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+	//'配置项'=>'配置值'
+
+    //给项目做静态文件访问路由路径的的配置
+    'CSS_URL'=>'/HOME/Public/style/',
+    'JS_URL'=>'/HOME/Public/js/',
+    'IMG_URL'=>'/HOME/Public/images/'
+
+
+);
---
+++ shop/index.php
@@ -0,0 +1,6 @@
+<?php
+//调试模式 以及生产模式
+define("APP_DEBUG",true);//调试模式
+//define("APP_DEBUG",false);//生产模式
+
+include("../ThinkPHP/ThinkPHP.php");
